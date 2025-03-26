85a4b5916f3b8e4c0ec1cd03aa346efd73602974
Darren Gonsalves <dgx@DGX-MBP.local>
2025-03-26 14:13:51 +0530

    font preload config changes

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,7 +1,13 @@
 import { Inter, Lusitana } from "next/font/google";
 
-export const inter = Inter({ subsets: ["latin"] });
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  preload: false,
+});
 export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
+  display: "swap",
+  preload: false,
 });
